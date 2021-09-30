b1e29cfd9e0e51e72cdcf9dfb0a3980c65559a4e
Alex Saxton <alex.saxton98@gmail.com>
2021-09-30 22:58:25 +0100

    added remove button to basket

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,66 @@ import './App.css';
 import basketIcon from "./images/basket-small.png"
 import Cat from "./components/catsapi"
 import { useEffect } from "react"
-const math = require("mathjs")
+import styled from "styled-components"
 
 
 function App() {
   const [basket, setBasket] = useState ([])
-  const [total, setTotal] = useState ([])
-  const [toggle, setToggle] = useState(false)
-  
+  const [total, setTotal] = useState (0)
+  const [toggleSwitch, setToggleSwitch] = useState(false)
 
   const handleClick = (data) => {
     let current = [...basket]
-    let currentTotal = [...total, data.price]
-    // currentTotal.push(data.price)
     current.push(data)
     setBasket(current)
     console.log(basket)
-    // let sum = 0
-    // for (let i = 0; i< currentTotal.length; i++){
-    //   sum += currentTotal[i]
-    // }
-    setTotal(currentTotal)
-   
-}
+    setTotal(total + parseInt(data.price))
+  }
 
+  const toggle = () => {
+      setToggleSwitch(!toggleSwitch)
+  } 
+  const handleRemove = (index, data) =>{
+    const basketUpdate = [...basket]
+    basketUpdate.splice(index, 1)
+    setBasket(basketUpdate)
+    setTotal(total - parseInt(data.price))
 
-  // console.log(Cat.data)
-  useEffect(() => {
-    document.title = "Cats4Lyfe"
-  }, [])
+  }
+
   return (
     <div className="app">
       <h1 className="pageTitle">Cats-4-Lyfe</h1>
-      <div className="basketIcon">
+      <div className="basketIcon" onClick={toggle}>
         <img src={basketIcon} alt="basket icon" />
-        
-        {basket.map((item) => (
-          <li>
-            {item.name}:
-            £{item.price}
-            <img id="image-small" src={item.url} alt="cat"/>
-          </li>
-        ))}
-        <p>Total: £{total}</p>
+      
+      {
+        toggleSwitch && (
+          <StyledContainer>
+              {basket.map((item, index) => (
+                <li className="listItems">
+                  {item.name}:
+                  £{item.price}
+                  <img id="image-small" src={item.url} alt="cat"/>
+                  <button onClick={()=>handleRemove(index, item)}>-</button>
+                </li>
+              ))}
+              <p>Total: £{total}</p>
+          </StyledContainer>
+        )
+      }
       </div>
+      <Cat handleClick={handleClick} />
       <Cat handleClick={handleClick} />
       <Cat handleClick={handleClick} />
       <Cat handleClick={handleClick} />
     </div>
   );
 }
+
+const StyledContainer = styled.div`
+  flex-direction: column;
+  background-color: #e0d1bf;
+`
 
 export default App;
