65c7512fc4e5d16197f4526a67a8ff09ca022391
Lorraine Williams <lorrainewilliams2604@gmail.com>
2021-09-30 10:05:43 +0100

    added favicon and title to tab bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@
 import './App.css';
 import basketIcon from "./images/basket-small.png"
 import Cat from "./components/catsapi"
+import { useEffect } from "react"
 
 function App() {
   // console.log(Cat.data)
+  useEffect(() => {
+    document.title = "Cats4Lyfe"
+  }, [])
   return (
     <div className="app">
       <h1 className="pageTitle">Cats-4-Lyfe</h1>
